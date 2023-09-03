3a530376a6699afa984ba69e02fe08a36ba1e641
Vishhvak Srinivasan <vishhvak@outlook.com>
2023-09-02 21:31:33 -0400

    Add user speech to text streaming capability, update circle to turn green when AI speaks

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ import * as O from "fp-ts/Option";
 import { useEffectOnce } from "react-use";
 import OpenAI from 'openai';
 
-const voiceID = "E83lgkQqxj1opeAo4NBd";
-const xiApiKey = "7b66194ad92fbf68e973456def29f632";
+const voiceID = process.env.NEXT_PUBLIC_VOICE_ID;
+const xiApiKey = process.env.NEXT_PUBLIC_ELEVEN_LABS_KEY;
 
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
@@ -28,7 +28,11 @@ export default function Index() {
   const [assistantMessage, setAssistantMessage] = useState('');
   const [audioPlaying, setAudioPlaying] = useState(false);
   const [fullAssistantMessage, setFullAssistantMessage] = useState('');
-  
+  const [userMessageStreaming, setUserMessageStreaming] = useState(false);
+  const [userMessageStreamed, setUserMessageStreamed] = useState(false);
+  const [assistantMessageFetched, setAssistantMessageFetched] = useState(false);
+
+
 
   const recentUserMessage = pipe(
     conversation,
@@ -49,7 +53,7 @@ export default function Index() {
     O.getOrElse(() => "user")
   );
 
-  console.log(conversation);
+  // console.log(conversation);
 
   const startRecording = () => {
     if (mediaRecorder.current && mediaRecorder.current.state !== "recording") {
@@ -94,13 +98,32 @@ export default function Index() {
             }
           );
 
-          setConversation((prevConversation) => [
-            ...prevConversation,
-            {
-              role: "user",
-              content: whisperResponse.data.text,
-            },
-          ]);
+          setConversation((prevConversation) => {
+            const newConversation = [
+              ...prevConversation,
+              {
+                role: "user",
+                content: whisperResponse.data.text,
+              },
+            ];
+
+            // Trigger chat completion immediately after updating the conversation
+            if (newConversation[newConversation.length - 1].role === "user") {
+              const main = async () => {
+                console.log("Calling OpenAI Chat completion with conversation - ", newConversation)
+                const response = await openai.chat.completions.create({
+                  model: 'gpt-3.5-turbo',
+                  messages: newConversation.map(message => ({ role: message.role, content: message.content })),
+                });
+                const assistantResponse = response.choices[0]?.message?.content || '';
+                setFullAssistantMessage(assistantResponse); // Store the full message
+                setChatCompleted(true);
+              };
+              main();
+            }
+
+            return newConversation;
+          });
         } catch (error) {
           console.error(error);
         }
@@ -109,30 +132,58 @@ export default function Index() {
   });
 
   useEffect(() => {
-    if (O.isSome(recentUserMessage) && currentTurn === "assistant") {
-      const main = async () => {
-        const response = await openai.chat.completions.create({
-          model: 'gpt-3.5-turbo',
-          messages: [{ role: 'user', content: recentUserMessage.value }],
-        });
-        const assistantResponse = response.choices[0]?.message?.content || '';
-        setFullAssistantMessage(assistantResponse); // Store the full message
-        setChatCompleted(true);
+    if (O.isSome(recentUserMessage) && !userMessageStreaming && !userMessageStreamed) {
+      setUserMessageStreaming(true);
+      const streamText = async () => {
+        let i = 0;
+        const animate = () => {
+          if (i < recentUserMessage.value.length) {
+            const partialMessage = recentUserMessage.value.slice(0, i + 1);
+            setConversation((prevConversation) => {
+              // If the last message is a user message, replace it with the new partial message
+              if (prevConversation.length > 0 && prevConversation[prevConversation.length - 1].role === "user") {
+                return [
+                  ...prevConversation.slice(0, prevConversation.length - 1),
+                  {
+                    role: "user",
+                    content: partialMessage,
+                  },
+                ];
+              }
+              // Otherwise, append the new partial message to the conversation
+              else {
+                return [
+                  ...prevConversation,
+                  {
+                    role: "user",
+                    content: partialMessage,
+                  },
+                ];
+              }
+            });
+            i++;
+            requestAnimationFrame(animate);
+          } else {
+            setUserMessageStreaming(false);
+            setUserMessageStreamed(true);
+          }
+        };
+        animate();
       };
-      main();
+      streamText();
     }
-  }, [conversation, currentTurn, recentUserMessage]);
-
+  }, [recentUserMessage, userMessageStreaming, userMessageStreamed]);
 
   useEffect(() => {
     if (
       O.isSome(recentUserMessage) &&
       currentTurn === "assistant" &&
       chatCompleted &&
-      !audioPlaying
+      !audioPlaying &&
+      userMessageStreamed &&
+      !assistantMessageFetched
     ) {
-      setAudioPlaying(true);
-
+      setAssistantMessageFetched(true);
       const fetchData = async () => {
         const response = await fetch(
           `https://api.elevenlabs.io/v1/text-to-speech/${voiceID}/stream`,
@@ -154,7 +205,7 @@ export default function Index() {
             }),
           }
         );
-
+      
         const reader = response.body.getReader();
         let chunks = [];
         return reader.read().then(function process({ done, value }) {
@@ -162,10 +213,16 @@ export default function Index() {
             const audioBlob = new Blob(chunks, { type: "audio/mpeg" });
             const audioUrl = window.URL.createObjectURL(audioBlob);
             const audio = new Audio(audioUrl);
-            audio.onplay = () => resolveAudioPlay(); // Resolve the promise when audio starts playing
+            audio.onplay = () => {
+              resolveAudioPlay(); // Resolve the promise when audio starts playing
+              setAudioPlaying(true); // Set audioPlaying to true when audio starts playing
+              streamText();
+            };
+            audio.onended = () => {
+              setAudioPlaying(false); // Set audioPlaying to false when audio finishes playing
+            };
             audio.play();
             setChatCompleted(false);
-            setAudioPlaying(false);
             return;
           }
           chunks.push(value);
@@ -207,9 +264,15 @@ export default function Index() {
         }
       };
 
-      Promise.all([fetchData(), streamText()]);
+      Promise.all([fetchData()]);
     }
   }, [chatCompleted, currentTurn, recentUserMessage, audioPlaying, fullAssistantMessage]);
+
+  useEffect(() => {
+    if (O.isSome(recentUserMessage) && currentTurn === "user") {
+      setAssistantMessageFetched(false); // Reset assistantMessageFetched when the user sends a new message
+    }
+  }, [recentUserMessage, currentTurn]);
 
   return (
     <div className={styles.page}>
@@ -219,7 +282,7 @@ export default function Index() {
       </Head>
 
       <div
-        className={styles.controls}
+        className={cx(styles.controls, { [styles.speaking]: audioPlaying })}
         onPointerDown={() => {
           startRecording();
         }}
@@ -228,12 +291,12 @@ export default function Index() {
         }}
       >
         <div
-          className={cx(styles.record, { [styles.active]: record })}
+          className={cx(styles.record, { [styles.active]: record, [styles.speaking]: audioPlaying })}
           type="button"
           disabled={!record}
         />
-        <div className={styles.copy}>
-          {record ? "Listening" : "Hold to speak"}
+        <div className={cx(styles.copy, { [styles.speaking]: audioPlaying })}>
+          {record ? "Listening" : (audioPlaying ? "Please wait, the AI is speaking." : "Hold to speak")}
         </div>
       </div>
 
